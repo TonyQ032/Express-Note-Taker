4f3a11fbd825e2d02dd5f4e46edbca67bcd608d9
aq032 <anthonyq032@gmail.com>
2022-08-03 12:17:58 -0700

    New object is created with a unique ID based on input field in note section

--- routes/dbRouter.js
+++ routes/dbRouter.js
@@ -2,10 +2,27 @@ const path = require('path');
 const router = require('express').Router();
 const db = require('../db/db.json');
 const uniqid = require('uniqid');
+const fs = require('fs');
 
 //GET route for reading and returning db.json
 router.get('/notes', (req, res) => {
   res.json(db);
+})
+
+//POST route for adding notes to db
+router.post('/notes', (req, res) => {
+  const {text, title} = req.body;
+
+  //console.log(text, title, "--------", req.body)
+
+  if (text, title) {
+    const completeNote = {
+      title,
+      text,
+      id: uniqid()
+    }
+    console.log(completeNote)
+  }
 })
 
 module.exports = router;
