bf1093b896bf7bdfffb7afa193db9adfb92ba443
aq032 <anthonyq032@gmail.com>
2022-08-03 13:24:23 -0700

    User notes are now successfully added to db.json. db.json emptied for use

--- routes/dbRouter.js
+++ routes/dbRouter.js
@@ -4,24 +4,42 @@ const db = require('../db/db.json');
 const uniqid = require('uniqid');
 const fs = require('fs');
 
-//GET route for reading and returning db.json
+// GET route for reading and returning db.json
 router.get('/notes', (req, res) => {
   res.json(db);
 })
 
-//POST route for adding notes to db
+// POST route for adding notes to db
 router.post('/notes', (req, res) => {
   const {text, title} = req.body;
 
   //console.log(text, title, "--------", req.body)
 
-  if (text, title) {
+  if (text && title) {
     const completeNote = {
       title,
       text,
       id: uniqid()
     }
-    console.log(completeNote)
+    //console.log(completeNote)
+
+    // fs function for adding a new object (completeNote) into db.json
+    fs.readFile('./db/db.json', 'utf-8', (err, data) => {
+      console.log("This is the file data" + data)
+      const dataObj = JSON.parse(data);
+      dataObj.push(completeNote);
+      const dataStr = JSON.stringify(dataObj);
+
+      fs.writeFile('./db/db.json', dataStr, (err) => err 
+      ? console.log(err) : console.log(`Note titled ${completeNote.title} has been added to database.`)
+      )
+    })
+
+    //Sends db to index.js for generating list on left
+    res.send(db);
+
+  } else {
+    res.status(500).json('Error in posting review!')
   }
 })
 
